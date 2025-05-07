f6db958feb3893cb23aac45c5ee0d336f50ba4d1
Kesh-2006-VNR <keshavans074@gmail.com>
2025-05-08 03:31:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -12,21 +12,3 @@ window.onscroll = function () {
   let scrolled = (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100;
   document.getElementById("progress-bar").style.width = scrolled + "%";
 };
-const form = document.querySelector("form");
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const name = form.name.value;
-  const email = form.email.value;
-  const message = form.message.value;
-
-  firebase.database().ref("messages").push({
-    name,
-    email,
-    message
-  });
-
-  alert("Message sent!");
-  form.reset();
-});
